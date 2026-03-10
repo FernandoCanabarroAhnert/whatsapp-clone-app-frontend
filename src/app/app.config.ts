4bfe01e4185edd0960f381bbde2db0341ed2e02c
FernandoCanabarroAhnert <ahnertfernando499@gmail.com>
2026-03-10 20:50:06 -0300

    config: adiciona configuração do Keycloak

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, inject, provideAppInitializer, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { KeycloakService } from './utils/keycloak/keycloak.service';
+import { keycloakInterceptor } from './utils/http/keycloak-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([keycloakInterceptor])),
+    provideAppInitializer(() => {
+      const initFn = ((kc: KeycloakService) => {
+        return () => kc.init();
+      })(inject(KeycloakService));
+      return initFn();
+    })
   ]
 };
